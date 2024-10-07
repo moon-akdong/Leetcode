0155723f73457e458ea1e9944e3d4839af4a33d8
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-10-08 00:42:48 +0900

    Time: 500 ms (99.9%), Space: 47.7 MB (99.48%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        q = []
+        node = head
+        rev = None
+        # slow-fast 러너 풀이 
+        fast = slow = node 
+        while fast and fast.next:
+            fast = fast.next.next
+            rev,rev.next,slow = slow,rev,slow.next
+        if fast:
+            slow = slow.next 
+        while rev and rev.val == slow.val:
+            slow,rev = slow.next, rev.next
+        return not rev 
