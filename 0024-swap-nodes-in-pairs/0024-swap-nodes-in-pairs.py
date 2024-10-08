04a5e8c1205eff097d9f06bda1f669dea2289927
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-10-08 20:25:00 +0900

    Time: 18 ms (25.43%), Space: 11.7 MB (55.72%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -9,21 +9,33 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        root=prev = ListNode(None)
-        # 연결리스트 이기 때문에
-        while head and head.next:
-            # 페어의 두번째 값 
-            b = head.next
-            # 페어가 가리키는 값이 head가 가리키게 만들기 1->3->4
-            head.next = b.next
-            # 순서 뒤바꾸기 2->1->3->4
-            b.next = head
-            
-            prev.next = b # None->2->1->.. 
-            head = head.next # 3->4
-            prev = prev.next.next # prev = 1 위치
+        if head and head.next:
+            p = head.next
+            # 백트래킹 되면서 넘어오는 곳 
+            head.next = self.swapPairs(p.next)
+            # 페어로 뒤집히는 곳 
+            p.next = head 
+            return p # 4->3->none 
 
-        return root.next if root.next else head
+        return head 
+
+        # 반복풀이 
+        # root=prev = ListNode(None)
+        # # 연결리스트 이기 때문에
+        # while head and head.next:
+        #     # 페어의 두번째 값 
+        #     b = head.next
+        #     # 페어가 가리키는 값이 head가 가리키게 만들기 1->3->4
+        #     head.next = b.next
+        #     # 순서 뒤바꾸기 2->1->3->4
+        #     b.next = head
+            
+        #     prev.next = b # None->2->1->.. 
+        #     head = head.next # 3->4
+        #     prev = prev.next.next # prev = 1 위치
+
+        # return root.next if root.next else head
+
 
             
         
