fbdc9fa64fc9eda9517352426da3882681c9e6c8
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-10-11 04:01:35 +0900

    Time: 840 ms (28.38%), Space: 20.9 MB (65.34%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,23 +4,23 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        profit = 0 
-        min_price = sys.maxsize
-        for price in prices:
-            min_price = min(min_price,price)
-            profit = max(profit,price-min_price)
-        return profit 
-        
-        # price = 0 
-        # max_num = 0
-        # min_num = sys.maxsize
-        # for i in prices:
-        #     max_num = max(i,max_num)
-        #     min_num = min(i,min_num)
-        #     if min_num == i:
-        #         max_num = i 
-        #     price = max_num - min_num 
-        # return price
+        # profit = 0 
+        # min_price = sys.maxsize
+        # for price in prices:
+        #     min_price = min(min_price,price)
+        #     profit = max(profit,price-min_price)
+        # return profit 
+
+        price = 0 
+        max_num = 0
+        min_num = sys.maxsize
+        for i in prices:
+            max_num = max(i,max_num)
+            min_num = min(i,min_num)
+            if min_num == i:
+                max_num = i 
+            price = max(price,max_num - min_num) 
+        return price
 
 
         
