75fc347cd0d936281eaf35b3eb2dceeaa3d35bc5
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-10-14 20:13:39 +0900

    Time: 199 ms (10.22%), Space: 13.1 MB (95.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,21 +4,43 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        stack = [] 
+        # two pointer solutions 
+        if not height:
+            return 0 
         water = 0 
-        for i in range(len(height)): # distance 때문에 range(len(height))
-        # 그리고 왠만한 상황 제외하고는 이렇게 시작하는게 좋은 듯 
-            while stack and height[i] > height[stack[-1]]:
-                pre = stack.pop() 
-                if not stack: # 빼버리고 없으면 append를 위해 
-                    break 
-                distance = i - stack[-1] - 1
-                # 같은 높이면 상쇄되서 volume = 0 이 됨 
-                volume = min(height[stack[-1]],height[i]) - height[pre]
-                water += distance * volume
-            stack.append(i)
+        left,right = 0,len(height)-1 
+        left_max,right_max = height[left],height[right]
+
+        while left<right:
+            left_max,right_max = max(height[left],left_max),\
+                                max(height[right],right_max)
+            if left_max <= right_max:
+                print(left_max,height[left])
+                water += left_max - height[left]
+                print('water',water)
+                left +=1 
+            else:
+                water += right_max - height[right]
+                right -=1 
         return water
 
-                
-                
+
+        # stack solutions
+        # stack = [] 
+        # water = 0 
+        # for i in range(len(height)): # distance 때문에 range(len(height))
+        # # 그리고 왠만한 상황 제외하고는 이렇게 시작하는게 좋은 듯 
+        #     while stack and height[i] > height[stack[-1]]:
+        #         pre = stack.pop() 
+        #         if not stack: # 빼버리고 없으면 append를 위해 
+        #             break 
+        #         distance = i - stack[-1] - 1
+        #         # 같은 높이면 상쇄되서 volume = 0 이 됨 
+        #         volume = min(height[stack[-1],height[i]) - height[pre]
+        #         water += distance * volume
+        #     stack.append(i)
+        # return water
+
+        # stack => 이전 꺼와 비교하여 값을 더해주는 것 
+        # two pointer => 진행하면서 더 작은 값이 나올 때 바로바로 더해주는 것 
 
