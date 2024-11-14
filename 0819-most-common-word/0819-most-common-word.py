382fc08bc0a32f5861777b53d711162cba19fc3b
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-11-14 16:06:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -8,19 +8,23 @@ class Solution(object):
         # 금지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라.
         # 대소문자 구분을 하지 않으며, 구두점 또한 무시한다.
 
-        # , 구두점 무시
-        paragraph = re.sub("[^a-zA-Z ]",' ',paragraph)
-        # 대소문자 구분 하지 않는다.
-        words = paragraph.lower().split()
+        # # , 구두점 무시
+        # paragraph = re.sub("[^\w]",' ',paragraph) # [^a-zA-Z ]
+        # # 대소문자 구분 하지 않는다.
+        # words = paragraph.lower().split()
 
-        words_counter = collections.Counter(words)
+        # words_counter = collections.Counter(words)
         
-        max_words = ""
-        counter = 0
-        for word,count in words_counter.items():
-            if word not in banned and counter < count:
-                counter = count
-                max_words = word
-        return max_words
+        # max_words = ""
+        # counter = 0
+        # for word,count in words_counter.items():
+        #     if word not in banned and counter < count:
+        #         counter = count
+        #         max_words = word
+        # return max_words
+
+        words = [word for word in re.sub("[\w]"," ",paragraph).lower().split() if word not in banned]
+        counts = collections.Counter(words)
+        return counts.most_common(1)[0][0]
 
         
