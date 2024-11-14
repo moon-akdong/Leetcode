2d1ccaca137522b2d38c1b20cc2a868bb3b50e8b
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-11-14 16:02:15 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        # 금지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라.
+        # 대소문자 구분을 하지 않으며, 구두점 또한 무시한다.
+
+        # , 구두점 무시
+        paragraph = re.sub("[^a-zA-Z ]",'',paragraph)
+        # 대소문자 구분 하지 않는다.
+        words = paragraph.lower().split()
+
+        words_counter = collections.Counter(words)
+        
+        max_words = ""
+        counter = 0
+        for word,count in words_counter.items():
+            print(word,count)
+            if word not in banned and counter < count:
+                counter = count
+                max_words = word
+        return max_words
+
+        
