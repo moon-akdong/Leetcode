525dc63dedd74403f1ec290684e584c4149f3411
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-11-14 16:23:40 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        # 문자열 배열을 받아 애너그램 단위로 그룹핑하라. 
+        # 알파벳이 동일하게 들어가 있는것들 
+
+        # 문자는 그대로 유지하면서, 같은 것 끼리 묶어야 한다. 
+        # 인덱스를 사용? 
+        result = [] 
+        words_group = collections.defaultdict(list)
+        for i in strs:
+            
+            word = sorted(i)
+            
+            if words_group not in words_group.keys():
+                words_group[''.join(word)].append(i)
+
+        return [words for words in words_group.values()]
