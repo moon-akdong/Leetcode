c6249010b664c0e289698c90acc9c05cbf469fa7
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-11-14 16:09:18 +0900

    Time: 4 ms (50.25%), Space: 11.9 MB (11.02%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -23,7 +23,7 @@ class Solution(object):
         #         max_words = word
         # return max_words
 
-        words = [word for word in re.sub("[\w]"," ",paragraph).lower().split() if word not in banned]
+        words = [word for word in re.sub("[^\w]"," ",paragraph).lower().split() if word not in banned]
         counts = collections.Counter(words)
         return counts.most_common(1)[0][0]
 
