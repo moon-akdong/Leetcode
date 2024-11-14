6c16f31559b1be99b2c87ec03f24ccef8e72b1ea
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-11-14 13:24:45 +0900

    Time: 8 ms (90.63%), Space: 14.4 MB (14.56%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,9 +6,17 @@ class Solution(object):
         """
         # 주어진 문자열이 팰린드롬인지 확인하라.
         # 대소문자를 구분하지 않으며, 영문자와 숫자만을 대상으로 한다
-        new_s = re.sub('[^0-9a-zA-Z]','',s)
-        array = []
-        for i in new_s:
-            array.append(i.lower())
-        return array == array[::-1]
+
+        # 1. 대소문자 구분하지 않음 
+        s = s.lower()
         
+        # 2. 영문자와 숫자만을 대상으로 한다. 
+        new_s = re.sub('[^0-9a-zA-Z]','',s)
+        # array = []
+        # for i in new_s:
+        #   if i.isalnum()     
+        #   array.append(i.lower())
+        
+        return new_s == new_s[::-1]
+        # 알고리즘이라고 할만 한게 없다. / deque와 list사용법, 또는 슬라이싱 사용 
+        # 대소문자 변경하는 방법과 정규표현식 정도
