61e95660a14629c3c10a4abc03628c7a5dd1d793
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-11-14 13:26:14 +0900

    Time: 0 ms (100%), Space: 19 MB (92.32%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        # 문자열을 뒤집는 함수를 작성하라. 입력값은 문자열 배열, 리턴 없이 리스트 내부를 직접 조작하라.
+        return s.reverse()
