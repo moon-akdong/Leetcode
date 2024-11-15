935358842cdebb16c1f0f04cf3bb1fbdad3fe886
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-11-15 13:00:55 +0900

    Time: 66 ms (97.1%), Space: 11.7 MB (47.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,21 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        # 가장 긴 팰린드롬 부분 문자열을 출력하라.  
-        center_ind = len(s) // 2
-        
+        # 중앙을 찾을 필요가 없다. -> 문장 내 팰리드롬이 있는지를 찾는 것 "abb"이런 예제가 있음 
+        # 투포인터 
         def expand(left,right):
-            while left >= 0 and right <= len(s) and s[left] == s[right-1]:
-                left -=1
+            while left>=0 and right <= len(s) and s[left] == s[right-1]:
+                left -= 1
                 right += 1
-            return s[left+1:right-1]
+            return s[left+1:right-1] # while 문으로 늘렸다가 아니면 이전꺼로 돌아가야 하므로
 
-        if len(s) < 2 or s==s[::-1]:
-            return s 
-        result = ''
+        if len(s) <2 or s == s[::-1]:
+            return s
+        result = ""
         for i in range(len(s)-1):
             result = max(result,
-            expand(i,i+1),
-            expand(i,i+2),
-            key = len)
+                            expand(i,i+1),
+                            expand(i,i+2),
+                            key=len)
         return result
