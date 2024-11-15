b32a8c357aa7aa970a8b4ad0b25cac3840bba0dd
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-11-15 13:22:18 +0900

    Time: 55 ms (97.76%), Space: 11.8 MB (25.84%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -8,6 +8,7 @@ class Solution(object):
         # 투포인터 
         def expand(left,right):
             while left>=0 and right <= len(s) and s[left] == s[right-1]:
+                # right<=len(s) "="를 붙힌 이유는 출력단에서-1을 해주기 때문에 
                 left -= 1
                 right += 1
             return s[left+1:right-1] # while 문으로 늘렸다가 아니면 이전꺼로 돌아가야 하므로
@@ -21,3 +22,6 @@ class Solution(object):
                             expand(i,i+2),
                             key=len)
         return result
+
+# 투포인터로 풀었고, i+1,i+2 로 짝수홀수에 대응 했다. 
+# max()함수에 key값으로 길이를 주어 길이가 가장 긴 것을 return 하게 했다.
