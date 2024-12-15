e6084e0ad97a0a2709a1ac9179b2e0e304599338
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-12-15 23:40:56 +0900

    Time: 15 ms (38.75%), Space: 13.3 MB (7.95%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,78 +1,104 @@
 class MyCircularDeque(object):
+
     def __init__(self, k):
         """
         :type k: int
         """
-        self.head, self.rear = ListNode(None),ListNode(None)
-        self.k,self.len = k,0
-        self.head.right, self.rear.left = self.rear, self.head 
-    def _add(self,node,new):
-        n = node.right 
-        node.right = new
-        new.left,new.right = node,n
-        n.left = new
-    def _del(self,node):
-        n = node.right.right
-        node.right=n
-        n.left = node
+        self.k,self.present = k,0
+        self.head,self.tail = ListNode(None),ListNode(None)
+        self.head.right, self.tail.left = self.tail, self.head 
+    
+    def _add (self,point,value):
+        prev = point.right # tail 
+        point.right = value
+        value.left,value.right = point,prev 
+        prev.left = value 
+
+    def _del(self,point):
+        prev = point.right.right
+        point.right = prev 
+        prev.left = point  
+
     def insertFront(self, value):
         """
         :type value: int
         :rtype: bool
         """
-        if self.len == self.k:
-            return False
-        self.len += 1
+        
+        if self.isFull():
+            return False 
         self._add(self.head,ListNode(value))
+        self.present +=1 
         return True
+
     def insertLast(self, value):
         """
         :type value: int
         :rtype: bool
         """
-        if self.len == self.k:
-            return False
-        self.len += 1 
-        self._add(self.rear.left,ListNode(value))
-        return True
+        if self.isFull():
+            return False 
+        self._add(self.tail.left,ListNode(value))
+        self.present +=1 
+        return True 
+
     def deleteFront(self):
         """
         :rtype: bool
         """
-        if self.len == 0:
-            return False
-        self.len -=1 
+        if self.isEmpty():
+            return False 
         self._del(self.head)
+        self.present-=1 
         return True
         
     def deleteLast(self):
         """
         :rtype: bool
         """
-        if self.len == 0:
-            return False
-        self.len -=1 
-        self._del(self.rear.left.left)
-        return True 
+        if self.isEmpty():
+            return False 
+        self._del(self.tail.left.left)
+        self.present-=1 
+        return True
+        
     def getFront(self):
         """
         :rtype: int
         """
-        return self.head.right.val if self.len else -1 
+        if self.isEmpty():
+            return -1 
+        return self.head.right.val
         
     def getRear(self):
         """
         :rtype: int
         """
-        return self.rear.left.val if self.len else -1 
-        
+        if self.present ==0:
+            return -1 
+        return self.tail.left.val
+
     def isEmpty(self):
         """
         :rtype: bool
         """
-        return self.len == 0
+        return self.present == 0
+        
+
     def isFull(self):
         """
         :rtype: bool
         """
-        return self.len == self.k
+        return self.present == self.k
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
