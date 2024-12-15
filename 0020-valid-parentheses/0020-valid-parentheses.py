7aa102860a1649f06de406ed9de2d3eeca2fa777
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-12-15 21:57:39 +0900

    Time: 4 ms (26.84%), Space: 12.2 MB (22.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -12,12 +12,16 @@ class Solution(object):
             ']':'[',
         }
 
-        stack = [] 
+        stack = []
         for char in s: 
+            # 열린 bracket이 들어올 때 
             if char not in table: # table.keys() 하면 더 느려짐 
                 stack.append(char)
+            # 닫힌 Bracket이 들어왔을 때 
+            # 해시맵 조회 및 비교 
             elif not stack or table[char] != stack.pop():
                 return False 
 
         return len(stack) == 0 
+        # 아래 주석이나 이거나 상관없음 단, 이게 조건이 뭔지 명확함
         # True if not stack else False 
