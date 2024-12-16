6d428d8d6dd0e29d3c0051893080f9095db3da4d
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-12-17 02:12:38 +0900

    Time: 19 ms (46.02%), Space: 13.3 MB (29.13%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,21 +4,20 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
-        left,right = 0,len(height)-1
-        left_max,right_max = height[left],height[right]
         volume = 0 
-        while left != right:
-            # 1. 큰 쪽으로 이동 하도록 좌우 비교해서 이동 시킨다. (two point방법)
-            # 2. 진행 중인 최대값을 유지하면서, 조회를 진행
-            left_max,right_max = max(height[left],left_max), \
-            max(height[right],right_max)
-            # 3. 
-            if left_max < right_max: 
-                volume += left_max - height[left]
-                left+=1 
-            else:
-                volume += right_max -height[right]
-                right-=1 
+        r = 0 
+        stack = [] 
+        while r < len(height):
+            while stack and height[r] > height[stack[-1]]:
+                top = stack.pop()
+                if not len(stack):
+                    break
+                distance  = r-stack[-1] -1 
+                waters = min(height[r],height[stack[-1]])-height[top]
+                volume += distance * waters
+            stack.append(r)
+            r += 1 
+        return volume 
 
-        return volume
+
+            
