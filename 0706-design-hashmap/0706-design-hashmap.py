6e1ab6f2da7ede12ae3d101e70a61c237f1b1f41
Jaeyun Moon <136424201+moon-akdong@users.noreply.github.com>
2024-12-16 22:26:16 +0900

    Time: 74 ms (60.74%), Space: 16.4 MB (35.69%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -3,12 +3,12 @@ class ListNode:
         self.key = key 
         self.value = val 
         self.next = None 
-
 class MyHashMap(object):
 
     def __init__(self):
+        self.size = 10000 
+        # At most 10^4 calls will be made to put, get, and remove.
         self.table = collections.defaultdict(ListNode)
-        self.size = 1000
 
     def put(self, key, value):
         """
@@ -16,17 +16,21 @@ class MyHashMap(object):
         :type value: int
         :rtype: None
         """
-        index = key % self.size
-        if self.table[index].value is None:
-            self.table[index] = ListNode(key,value)
+        hashKey = key % self.size 
+        # 비어 있다면 삽입 
+        
+        if self.table[hashKey].value is None:
+            self.table[hashKey] = ListNode(key,value)
             return 
-        p = self.table[index]
+        p = self.table[hashKey]
         while p:
+            # 키가 동일 하다면 업데이트 
             if p.key == key:
-                p.value = value 
+                p.value = value
                 return 
+        # 동일하지 않다면 연결리스트 다음에 넣어주기 
             if p.next is None:
-                break 
+                break
             p = p.next
         p.next = ListNode(key,value)
 
@@ -35,38 +39,44 @@ class MyHashMap(object):
         :type key: int
         :rtype: int
         """
-        index = key % self.size
-        # 조회 하려는 키가 없는 경우 
-        if self.table[index].value is None:
+        hashKey = key % self.size
+        if self.table[hashKey].value is None:
             return -1 
-        # 키가 있는 경우
-        p = self.table[index]
+        
+        p = self.table[hashKey]
         while p:
             if p.key == key:
                 return p.value 
+            if p.next is None:
+                return -1 
             p = p.next
-        return -1 
+        
 
     def remove(self, key):
         """
         :type key: int
         :rtype: None
         """
-        index = key % self.size 
-        if self.table[index].value is None:
+        hashKey = key % self.size
+        # 아예 없는 경우
+        if self.table[hashKey].value is None:
             return 
-        # 인덱스의 첫 번째 노드일 때 삭제 처리
-        p = self.table[index]
+        # 있는데, 충돌이 없었던 key 
+        p = self.table[hashKey]
         if p.key == key:
-            self.table[index] = ListNode() if p.next is None else p.next
+            self.table[hashKey] = ListNode(None) if p.next is None else p.next
             return 
-        
-        prev = p 
+        # 있는데, 충돌이 있었어서 뒤에꺼도 확인해야 되는 경우
+        prev = p
         while p:
             if p.key == key:
                 prev.next = p.next 
                 return 
             prev,p = p, p.next 
+        
+            
+        
+
         
 
 
